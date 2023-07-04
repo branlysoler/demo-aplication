964ccdca82fee9554255471e1e6db7e3147cde9e
Branly <branlysolermartinez@gmail.com>
2023-07-04 11:03:30 -0400

    now

--- demo-service/src/main/java/com/example/demoservice/controller/SuplyController.java
+++ demo-service/src/main/java/com/example/demoservice/controller/SuplyController.java
@@ -4,7 +4,6 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,10 +16,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demoservice.dto.SuplyDTO;
 import com.example.demoservice.feign_clients.OtherDemoFeignClient;
-import com.example.demoservice.util.Text;
 
 import feign.FeignException;
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 
 @RestController
 @RequestMapping("/demo-service/suplies")
@@ -31,7 +28,6 @@ public class SuplyController {
 
     private static final Log LOGGER = LogFactory.getLog(SuplyController.class);
 
-    @CircuitBreaker(name = "suplyCB", fallbackMethod = "fallbackCreateOrUpdate")
     @PostMapping("/create")
     public ResponseEntity<?> create(@RequestBody SuplyDTO suplyDTO) {
         LOGGER.info("Create Suply: " + suplyDTO.toString());
@@ -42,7 +38,6 @@ public class SuplyController {
         }
     }
 
-    @CircuitBreaker(name = "suplyCB", fallbackMethod = "fallbackCreateOrUpdate")
     @PutMapping("/update")
     public ResponseEntity<?> update(@RequestBody SuplyDTO suplyDTO) {
         try {
@@ -52,13 +47,11 @@ public class SuplyController {
         }
     }
 
-    @CircuitBreaker(name = "suplyCB", fallbackMethod = "fallbackFindAll")
     @GetMapping("/findAll")
     public ResponseEntity<?> findAll(Pageable pageable) {
         return otherDemoFeignClient.findAll(pageable);
     }
 
-    @CircuitBreaker(name = "suplyCB", fallbackMethod = "fallbackFindById")
     @GetMapping("/findById/{id}")
     public ResponseEntity<?> findById(@PathVariable Long id) {
         try {
@@ -68,7 +61,6 @@ public class SuplyController {
         }
     }
 
-    @CircuitBreaker(name = "suplyCB", fallbackMethod = "fallbackFindByName")
     @GetMapping("/findByName/{name}")
     public ResponseEntity<?> findByName(@PathVariable String name) {
         try {
@@ -78,7 +70,6 @@ public class SuplyController {
         }
     }
 
-    @CircuitBreaker(name = "suplyCB", fallbackMethod = "fallbackDelete")
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<?> deleteById(@PathVariable Long id) {
         try {
@@ -88,37 +79,9 @@ public class SuplyController {
         }
     }
 
-    @CircuitBreaker(name = "suplyCB", fallbackMethod = "fallbackDeleteAll")
     @DeleteMapping("/deleteAll")
     public ResponseEntity<?> deleteAll() {
         return otherDemoFeignClient.deleteAll();
-    }
-
-    /***********************************/
-    /* methods fallback circuitBreaker */
-    /***********************************/
-    public ResponseEntity<?> fallbackCreateOrUpdate(@RequestBody SuplyDTO suplyDTO, RuntimeException e) {
-        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(Text.MICROSERVICE_NOT_AVAILABLE);
-    }
-
-    public ResponseEntity<?> fallbackFindAll(Pageable pageable, RuntimeException e) {
-        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(Text.MICROSERVICE_NOT_AVAILABLE);
-    }
-
-    public ResponseEntity<?> fallbackFindById(@PathVariable Long id, RuntimeException e) {
-        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(Text.MICROSERVICE_NOT_AVAILABLE);
-    }
-
-    public ResponseEntity<?> fallbackFindByName(@PathVariable String name, RuntimeException e) {
-        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(Text.MICROSERVICE_NOT_AVAILABLE);
-    }
-
-    public ResponseEntity<?> fallbackDelete(@PathVariable Long id, RuntimeException e) {
-        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(Text.MICROSERVICE_NOT_AVAILABLE);
-    }
-
-    public ResponseEntity<?> fallbackDeleteAll(RuntimeException e) {
-        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(Text.MICROSERVICE_NOT_AVAILABLE);
     }
 
 }
